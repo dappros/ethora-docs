2dfebea91e17bc7ed3c7b5acde8b4e2c6d96b733
Nikhil Bankar <nikhilbankar1994@gmail.com>
2024-07-23 13:00:07 +0530

    Added dynamic title and open graph meta tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -6,13 +6,30 @@ export default {
   logo: <img src="https://www.ethora.com/images/ethora-logo-new.png" alt="Ethora Logo" width="130px" />,
   head: (
     <>
-      <title>Ethora Docs</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-      <meta name="description" content="Ethora Documentation" />
-      <meta name="og:title" content="Ethora Docs" />
+      <meta name="title" content="Ethora Documentation" />
+      <meta name="description" content="Ethora is low-code and highly customisable which means you can launch your own branded app into appstore in no time. Follow the documentation for more insights." />
       <link rel="icon" href="https://www.ethora.com/images/favicon_io/favicon.ico" />
+     
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://docs.ethora.com/" />
+      <meta property="og:title" content="Ethora Documentation" />
+      <meta property="og:description" content="Ethora is low-code and highly customisable which means you can launch your own branded app into appstore in no time. Follow the documentation for more insights." />
+      <meta property="og:image" content="/og-ethora.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://docs.ethora.com/" />
+      <meta property="twitter:title" content="Ethora Documentation" />
+      <meta property="twitter:description" content="Ethora is low-code and highly customisable which means you can launch your own branded app into appstore in no time. Follow the documentation for more insights." />
+      <meta property="twitter:image" content="/og-ethora.png" />
     </>
   ),
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: 'Ethora Docs - %s'
+    }
+  },
 
   feedback:{
     content: "Questions? Visit our forum",
